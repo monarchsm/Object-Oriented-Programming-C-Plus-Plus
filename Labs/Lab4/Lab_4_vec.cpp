a41c0e53bd370b558c54516e6bd86ecd96ef2cf6
monarchsm <jjlizmm@gmail.com>
2023-10-30 15:39:13 -0700

    Beginning Information (basic info)

---
+++ Labs/Lab4/Lab_4_vec.cpp
@@ -0,0 +1,36 @@
+/***
+ * CSE 2010 Fall 2023
+ * Lab #4
+ * Lizbeth Jimenez #007670332
+ * 30 Ocotober 2023
+ *
+ * To learn the differences of arrays and vectors in detail, to how they contain elements and 
+ * how to pass them into functions. And ultimately learn how to organize orangize data with data 
+ * structures. 
+ * 
+ *
+ * {Describe 1) what was most challenging and 2) how you solved/overcame the problem OR
+ * the directions of how you PLAN to solve it}
+ *
+***/
+
+#include <iostream>
+#include "ArrayVec.h"
+#include "DisplayVec.h"
+using namespace std;
+
+
+
+int main()
+{
+    // initialize vec
+
+    //initialize ArrayVec and DisplayVec
+
+
+    // populate with values
+
+    // display contents
+
+}
+
