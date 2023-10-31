d9c36e1570f21676e12fb0543bec16be6b05b459
monarchsm <jjlizmm@gmail.com>
2023-10-30 21:07:20 -0700

    Updated Initial Intro

--- Labs/Lab4/Lab_4_vec.cpp
+++ Labs/Lab4/Lab_4_vec.cpp
@@ -2,15 +2,14 @@
  * CSE 2010 Fall 2023
  * Lab #4
  * Lizbeth Jimenez #007670332
- * 30 Ocotober 2023
+ * 30 October
  *
  * To learn the differences of arrays and vectors in detail, to how they contain elements and 
  * how to pass them into functions. And ultimately learn how to organize orangize data with data 
- * structures. 
- * 
+ * structures.
  *
- * {Describe 1) what was most challenging and 2) how you solved/overcame the problem OR
- * the directions of how you PLAN to solve it}
+ * The most challenging for me was remembering how to run my terminal. My solution is to open lab 
+ * lectures and run it. 
  *
 ***/
 
